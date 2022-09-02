df3e5092a5550554fc87fd8ef0562b2973cc1689
gouder hicham <gouderhicham@gmail.com>
2022-09-02 16:51:03 +0100

    remove unnecessery code

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -3,25 +3,18 @@ import { useEffect } from "react";
 const Nav = () => {
   const [input, setinput] = useState("");
   const [filteredOptions, setfilteredOptions] = useState([]);
-  const [locs, setlocs] = useState([]);
-  const [options, setoptions] = useState([
-    { name: "hicham" },
-    { name: "gouder" },
-    { name: "haithem" },
-    { name: "sara" },
-    { name: "safa" },
-    { name: "sabrin" },
-  ]);
+  const [locations, setlocations] = useState([]);
   async function getLocations(input) {
     if (input === "") return;
     let res = await fetch(
       `https://api.geoapify.com/v1/geocode/autocomplete?text=${input}&apiKey=40de237fa18940ff8eb30ef323a52d08`
     );
     let { features } = await res.json();
-    setlocs(features);
+    setlocations(features);
   }
   useEffect(() => {
     getLocations(input);
+    // console.log(locations[0].properties.address_line2);
   }, [input]);
   return (
     <nav>
@@ -34,33 +27,22 @@ const Nav = () => {
             onChange={(e) => {
               // change the input value to the input field
               setinput(e.target.value);
-              // change the filtered loactions based on the input value as it's changing
-              let filterdLocations = options.filter((name) => {
-                if (
-                  name.name.indexOf(e.target.value) !== -1 &&
-                  e.target.value !== ""
-                ) {
-                  return { value: name.name };
-                }
-              });
-              // change the filtered options state
-              setfilteredOptions(filterdLocations);
             }}
             type="text"
           />
           <div className="suggestions">
-            {filteredOptions.length > 0 &&
-              filteredOptions.map((option) => (
+            {locations.length > 0 &&
+              locations.map((option) => (
                 <div
                   onClick={(e) => {
                     // change the input field to the suggested option clicked
                     setinput(e.target.textContent);
                     // change the options to null after clicking on one to hide the options field
-                    setfilteredOptions([]);
+                    setlocations([]);
                   }}
                   key={option.name}
                 >
-                  {option.name}
+                  {option.properties.address_line1}
                 </div>
               ))}
           </div>
