ca5f501dc59ad1540023c4e5e93a476a83f17c15
gouder hicham <gouderhicham@gmail.com>
2022-09-08 18:29:23 +0100

    add comments

--- src/exports.jsx
+++ src/exports.jsx
@@ -1,7 +1,11 @@
 import star from "./images/star.png";
 import halfStar from "./images/rating.png";
+
+// places autocomplete function by fetching places from geopify api based on input
 export const placesUrl = (input) =>
   `https://api.geoapify.com/v1/geocode/autocomplete?text=${input}&apiKey=40de237fa18940ff8eb30ef323a52d08`;
+
+// options needed by rapid api 
 export const options = {
   method: "GET",
   headers: {
@@ -9,12 +13,14 @@ export const options = {
     "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
   },
 };
+// fetch restaurants by bounds from travel advisor api 
 export const restaurantsUrl = (BLat, BLng, TLat, TLng) =>
   `https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary?bl_latitude=${BLat}&tr_latitude=${TLat}&bl_longitude=${BLng}&tr_longitude=${TLng}&limit=30&currency=USD&open_now=false&lunit=km&lang=en_US`;
-
+// fetch hotels by bounds from travel advisor api 
 export const hotelsUrl = (BLat, BLng, TLat, TLng) =>
   `https://travel-advisor.p.rapidapi.com/hotels/list-in-boundary?bl_latitude=${BLat}&bl_longitude=${BLng}&tr_longitude=${TLng}&tr_latitude=${TLat}&limit=30&currency=USD`;
 
+// display rating from number (ex: 4.2) to stars
 export function getRating(rating) {
   let arr = [];
   for (let i = 0; i < Math.floor(rating); i++) {
